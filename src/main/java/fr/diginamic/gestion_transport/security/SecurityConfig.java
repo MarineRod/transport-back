1b9ba624adbbdb2dd41e86409a5cff910e2faaba
EmanuelCottarel <emanuelcottarel@gmail.com>
2025-05-05 12:09:01 +0200

    1: Config CORS

--- src/main/java/fr/diginamic/gestion_transport/security/SecurityConfig.java
+++ src/main/java/fr/diginamic/gestion_transport/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
 
 /**
  * Configure la sécurité
@@ -36,6 +37,7 @@ public class SecurityConfig {
 	    http.csrf(AbstractHttpConfigurer::disable) // Désactiver CSRF
 	        .authorizeHttpRequests(authorize -> authorize
 	            .requestMatchers("/api/auth/**").permitAll() // Autoriser /api/auth sans authentification
+				.requestMatchers(CorsUtils::isPreFlightRequest).permitAll() // Autoriser toutes les requêtes preflight pour éviter les CORS
 	            .anyRequest().authenticated() // Tous les autres endpoints sont authentifiés
 	        );
 	    http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
