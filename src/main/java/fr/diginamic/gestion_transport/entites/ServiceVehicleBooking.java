cac3e080338e3baf9c626d9ab7fd877d69bed331
Marine Rodriguez <marine.rodriguez@wanadoo.fr>
2025-05-05 22:42:22 +0200

    Revert "#13: création du back"
    This reverts commit 108b87dce904c58dc9174fa829d55012fedd9728.

--- src/main/java/fr/diginamic/gestion_transport/entites/ServiceVehicleBooking.java
+++ src/main/java/fr/diginamic/gestion_transport/entites/ServiceVehicleBooking.java
@@ -5,11 +5,12 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-//@Getter
-//@Setter
-//@Builder
-//@NoArgsConstructor
-//@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 @Entity
 public class ServiceVehicleBooking {
 
@@ -20,6 +21,7 @@ public class ServiceVehicleBooking {
 	@Column(nullable = false)
 	private LocalDateTime dateTimeEnd;
 
+
 	@ManyToOne
 	@JoinColumn(name = "SERVICE_VEHICLE_ID")
 	private ServiceVehicle serviceVehicle;
@@ -27,59 +29,4 @@ public class ServiceVehicleBooking {
 	@ManyToOne
 	@JoinColumn(name = "USER_ID")
 	private User user;
-
-	public ServiceVehicleBooking() {
-
-	}
-
-	public ServiceVehicleBooking(Integer id, LocalDateTime dateTimeStart, LocalDateTime dateTimeEnd,
-			ServiceVehicle serviceVehicle, User user) {
-		super();
-		this.id = id;
-		this.dateTimeStart = dateTimeStart;
-		this.dateTimeEnd = dateTimeEnd;
-		this.serviceVehicle = serviceVehicle;
-		this.user = user;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public LocalDateTime getDateTimeStart() {
-		return dateTimeStart;
-	}
-
-	public void setDateTimeStart(LocalDateTime dateTimeStart) {
-		this.dateTimeStart = dateTimeStart;
-	}
-
-	public LocalDateTime getDateTimeEnd() {
-		return dateTimeEnd;
-	}
-
-	public void setDateTimeEnd(LocalDateTime dateTimeEnd) {
-		this.dateTimeEnd = dateTimeEnd;
-	}
-
-	public ServiceVehicle getServiceVehicle() {
-		return serviceVehicle;
-	}
-
-	public void setServiceVehicle(ServiceVehicle serviceVehicle) {
-		this.serviceVehicle = serviceVehicle;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
 }
--- src/main/java/fr/diginamic/gestion_transport/entites/Vehicle.java
+++ src/main/java/fr/diginamic/gestion_transport/entites/Vehicle.java
@@ -2,10 +2,7 @@ package fr.diginamic.gestion_transport.entites;
 
 import fr.diginamic.gestion_transport.enums.BrandEnum;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
@@ -30,60 +27,4 @@ public class Vehicle {
 
 	@OneToMany(mappedBy = "vehicle")
 	private List<Carpooling> carpoolings;
-
-	public Vehicle() {
-
-	}
-
-	public Vehicle(String licensePlateNumber, String model, Integer nbSeats, BrandEnum brand,
-			List<Carpooling> carpoolings) {
-		super();
-		this.licensePlateNumber = licensePlateNumber;
-		this.model = model;
-		this.nbSeats = nbSeats;
-		Brand = brand;
-		this.carpoolings = carpoolings;
-	}
-
-	public String getLicensePlateNumber() {
-		return licensePlateNumber;
-	}
-
-	public void setLicensePlateNumber(String licensePlateNumber) {
-		this.licensePlateNumber = licensePlateNumber;
-	}
-
-	public String getModel() {
-		return model;
-	}
-
-	public void setModel(String model) {
-		this.model = model;
-	}
-
-	public Integer getNbSeats() {
-		return nbSeats;
-	}
-
-	public void setNbSeats(Integer nbSeats) {
-		this.nbSeats = nbSeats;
-	}
-
-	public BrandEnum getBrand() {
-		return Brand;
-	}
-
-	public void setBrand(BrandEnum brand) {
-		Brand = brand;
-	}
-
-	public List<Carpooling> getCarpoolings() {
-		return carpoolings;
-	}
-
-	public void setCarpoolings(List<Carpooling> carpoolings) {
-		this.carpoolings = carpoolings;
-	}
-
-	
 }
