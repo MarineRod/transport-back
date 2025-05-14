6f87297f5cbbf548401d46ef0c48892ffde7c6dd
EmanuelCottarel <emanuelcottarel@gmail.com>
2025-05-14 10:43:57 +0200

    Implementation CICD

---
+++ src/test/java/fr/diginamic/gestion_transport/controllers/CarpoolingControllerTest.java
@@ -0,0 +1,14 @@
+package fr.diginamic.gestion_transport.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarpoolingControllerTest {
+
+    @Test
+    public void testCarpoolingController() {
+        assertTrue(true);
+    }
+
+}
