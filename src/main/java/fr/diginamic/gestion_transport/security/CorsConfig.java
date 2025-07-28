1369a7e8c552949c16f650ccc12bccb8ff3f68ee
MarineRod <146340117+MarineRod@users.noreply.github.com>
2025-07-28 18:36:31 +0200

    Update CorsConfig.java

--- src/main/java/fr/diginamic/gestion_transport/security/CorsConfig.java
+++ src/main/java/fr/diginamic/gestion_transport/security/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://gestion-transport-front.vercel.app/")
+                        .allowedOrigins("https://gestion-transport-front.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
